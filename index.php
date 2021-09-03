8a08d4aa991b88f0ad4306ce545b51c31b96f3f0
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2021-09-03 08:51:01 +0530

    Added Background

--- index.php
+++ index.php
@@ -5,10 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Cratonik Banking</title>
+    <link rel="stylesheet" href="./assets/css/style.css" />
   </head>
   <body>
-      <?php
-        echo '<h1>Hello</h1>'
-      ?>
+    <section class="home"></section>
   </body>
 </html>
