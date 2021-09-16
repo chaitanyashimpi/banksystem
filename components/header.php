8228a73db3b3ebc7dd97b297f91be707b7da99f4
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2021-09-16 21:44:05 +0530

    Changed something

---
+++ components/header.php
@@ -0,0 +1,9 @@
+<header>
+  <nav>
+    <div class="logo">
+      <span class="logoDesign"></span>
+      <h1>CRT Bank</h1>
+    </div>
+    
+  </nav>
+</header>
--- index.php
+++ index.php
@@ -5,9 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Cratonik Banking</title>
-    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/styles.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
+      rel="stylesheet"
+    />
   </head>
   <body>
-    <section class="home"></section>
+    <?php 
+      include "./components/header.php";
+    ?>
+    <section class="home">
+      <h1>
+        All the <span>experience</span><br />
+        in new <i class="ri-arrow-right-line"></i>CRT Bank
+      </h1>
+    </section>
   </body>
 </html>
