3f8496acd53bd84c8f3864b7e937196101acca23
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2021-09-18 20:00:34 +0530

    Changed CSS file name

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Cratonik Banking</title>
-    <link rel="stylesheet" href="./assets/css/styles.css" />
+    <link rel="stylesheet" href="./assets/css/stylesheet.css" />
     <link
       href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css"
       rel="stylesheet"
