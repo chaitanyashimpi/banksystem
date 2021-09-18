80b502d91527d7bddc4be14381f413970e902415
Chaitanya Shimpi <chaitanyashimpi2001@gmail.com>
2021-09-18 23:44:19 +0530

    Added main component

---
+++ components/all-in-one-component.php
@@ -0,0 +1,15 @@
+<div class="compNav">
+          <h1>CRT Banking</h1>
+
+          <ul>
+            <a href="create-user.php">
+              <li>Create User</li>
+            </a>
+            <a href="#">
+              <li>Transfer Money</li>
+            </a>
+            <a href="#">
+              <li>Transaction History</li>
+            </a>
+          </ul>
+        </div>
--- create-user.php
+++ create-user.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/create-user.css">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./assets/css/create-user.css" />
     <title>CRT Bank | Create User</title>
-    
-</head>
-<body>
+  </head>
+  <body>
     <?php 
       include "./components/header.php";
     ?>
-    <div class="createUser">
-
-    </div>
+    <main class="crtbanking">
+      <div class="crtComponent">
+        <?php
+            include "./components/all-in-one-component.php"
+        ?>
+        <div class="compMainNav">h2</div>
+      </div>
+    </main>
     <?php 
         include './components/footer.php';
     ?>
-</body>
+  </body>
 </html>
